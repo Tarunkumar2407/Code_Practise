e46abfbfe8ac82099dbeb5aaf6a7e71bae7024ad
Tarun Kumar <tarunkumar2407@gmail.com>
2024-02-16 22:21:37 +0530

    initial commit

--- Practise 1/script.js
+++ Practise 1/script.js
@@ -28,9 +28,126 @@ console.log("hello world")
 // var age;
 // console.log(age)
 
-let student1 = Symbol("Tarun")
-let student2 = Symbol("Tarun")
-console.log(student1 == student2)
-console.log(student1)
-// alert(student1.toString())
-console.log(student1.description)
+// let student1 = Symbol("Tarun")
+// let student2 = Symbol("Tarun")
+// console.log(student1 == student2)
+// console.log(student1)
+// // alert(student1.toString())
+// console.log(student1.description)
+
+// const age = 24;
+// const drink = age <= 5 ? "coffee" : "tea"
+// console.log(drink)
+
+//switch case 
+
+// const marks = "C";
+// switch(marks) {
+//     case "A" : 
+//          console.log("superb")
+//          break;
+//     case "B" :
+//          console.log("Perfect")
+//          break;
+//     case "C" :
+//          console.log("Good")
+//          break;
+//     case "D" : 
+//          console.log("Fine")
+//          break;
+//     default : console.log("Check Your Result Carefully")
+// }
+
+
+// for(let index = 0; index<5 ; index++){
+//     if(index == 2){
+//         continue;
+//     }
+//     console.log(index, "Tarun")
+// }
+
+// let step = 0;
+// while(step<5){
+//     step++;
+//     if(step == 1){
+//         continue;
+//     }
+//     console.log(step, "step")
+  
+//     // if(step == 2){
+//     //     break;
+//     // }
+    
+// }
+
+// let num = 6; 
+// do{
+//     console.log(num, "step")
+//     num++;
+// }while(num<5)
+
+// let arr = ["rd Sharma", "rs agarwal", "chemistry"];
+// console.log(arr)
+// console.log(arr[1])
+// console.log(arr[0])
+// console.log(arr[2])
+// arr.push("mathematics")
+// console.log(arr)
+
+// arr.pop()
+// console.log(arr)
+
+// arr.unshift("biology")
+// console.log(arr)
+
+// arr.shift();
+// console.log(arr)
+
+// arr[2] = "Physics"
+// console.log(arr)
+
+// let arr2 = arr.slice(0,2)
+// console.log(arr2)
+
+// let arr = ["Arun", "Ankur", "Manu", "Tanu"]
+// console.log(arr)
+
+// arr.splice(1,2, "Raj", "Manoj")
+// console.log(arr)
+
+// const marks = [23, 24, 26, 28, 32];
+// console.log(marks)
+// const marks2 = marks;
+// marks.push(48)
+// console.log(marks)
+// console.log(marks2)
+// console.log(marks == marks2)
+
+// const arr = [];
+// const arr1 = [];
+
+// console.log(arr == arr1)
+
+// const arr = ["Tarun", "Ankur", "Sumer"];
+// // for(let key in arr){
+// //     console.log(arr[key])
+// // }
+
+// console.log(Array.isArray(arr))
+// arr.sort()
+// console.log(arr)
+
+// console.log(arr1.sort((a,b) => a - b))
+// console.log(arr1.sort((a,b) => b - a))
+// const arr1= [84, 56, 75, 589, 875, 25, 24];
+
+// console.log(arr1.join("and"))
+
+//cloning of array
+
+// const arr1 = ["item1", "item2", "item3"]
+// const arr2 = ["item1", "item2", "item3"]
+// const arr3 = [].concat(arr1)
+// console.log(arr3)
+// const arr4 = [...arr1]
+// console.log(arr4)
