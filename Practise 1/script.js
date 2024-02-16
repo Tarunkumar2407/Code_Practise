0e8125457463fdf268e8d0c027eae95272d1d885
Tarun Kumar <tarunkumar2407@gmail.com>
2024-02-16 20:21:29 +0530

    initial commit

--- Practise 1/script.js
+++ Practise 1/script.js
@@ -1,1 +1,36 @@
 console.log("hello world")
+// document.write(6)
+// document.write("Hello World!");
+// window.alert("Hello world")
+// alert("Hello world")
+
+// console.log(null + 6)
+// console.log(undefined + 1)
+// "use strict"
+// a = 6; 
+// console.log(a)
+
+// console.log(typeof a)
+
+// console.log(name)
+// var name;
+// console.log(name)
+
+// var name;
+// console.log(name)
+
+
+// var carName;
+// console.log(carName)
+// console.log(typeof carName)
+
+
+// var age;
+// console.log(age)
+
+let student1 = Symbol("Tarun")
+let student2 = Symbol("Tarun")
+console.log(student1 == student2)
+console.log(student1)
+// alert(student1.toString())
+console.log(student1.description)
